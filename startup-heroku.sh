180a24f863e5a48435da414dd5ae120c6296fb07
Phillip Verheyden <pverheyden@gmail.com>
2012-08-21 12:00:06 -0500

    Added ability to deploy the admin application alongside the site, ensured to remove extraneous webapps bundled with the tomcat cargo

--- startup-heroku.sh
+++ startup-heroku.sh
@@ -2,8 +2,16 @@
 CATALINA_BASE=`pwd`/site/target/cargo/configurations/tomcat7x
 export CATALINA_BASE
 
+#move the admin application to the tomcat deploy
+mv ./admin/target/admin.war $CATALINA_BASE/webapps
+
+#remove the extraneous webapps that we don't need
+rm -rf $CATALINA_BASE/webapps/cargocpc*
+rm -rf $CATALINA_BASE/host-manager
+rm -rf $CATALINA_BASE/manager
+
 #copy over the Heroku config files
-cp ./site/heroku-server.xml ./site/target/cargo/configurations/tomcat7x/conf/server.xml
+cp ./site/heroku-server.xml $CATALINA_BASE/conf/server.xml
 
 #make the Tomcat scripts executable
 chmod a+x ./site/target/cargo/installs/apache-tomcat-7.0.29/apache-tomcat-7.0.29/bin/*.sh
