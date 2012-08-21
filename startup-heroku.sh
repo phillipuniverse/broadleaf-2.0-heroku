86e7963b512ffa29d9bb8c417bd4bce9725b7689
Phillip Verheyden <pverheyden@gmail.com>
2012-08-21 10:19:14 -0500

    Added local/heroku Procfiles, added local/heroku startups

---
+++ startup-heroku.sh
@@ -0,0 +1,16 @@
+#point to the correct configuration and webapp
+CATALINA_BASE=`pwd`/site/target/cargo/configurations/tomcat7x
+export CATALINA_BASE
+
+#copy over the Heroku config files
+cp ./site/heroku-server.xml ./site/target/cargo/configurations/tomcat7x/conf/server.xml
+
+#make the Tomcat scripts executable
+chmod a+x ./site/target/cargo/installs/apache-tomcat-7.0.29/apache-tomcat-7.0.29/bin/*.sh
+
+#set the correct port and database settings
+JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=256M -Xmx256M -Dhttp.port=$PORT -DDATABASE_URL=$DATABASE_URL"
+export JAVA_OPTS
+
+#start Tomcat
+./site/target/cargo/installs/apache-tomcat-7.0.29/apache-tomcat-7.0.29/bin/catalina.sh run
